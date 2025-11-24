e5f64fabdcb81dbe0df13ef20c39b4d99ca90315
Rakoto <rakotonavalona206@gmail.com>
2025-11-24 10:53:34 +0300

    Sprint7
    Creation de GetUrl et PostUrl et renommet l'annotation general Get en RequestUrl puis ajout de httpMethod dans AnnotationMapping

--- src/main/java/com/projet_framework/annotation/mapper/AnnotationMapping.java
+++ src/main/java/com/projet_framework/annotation/mapper/AnnotationMapping.java
@@ -6,38 +6,54 @@ public class AnnotationMapping {
     private Class<?> clazz;
     private Method method;
     private String url;
-    
-    
-    public AnnotationMapping(Class<?> clazz, Method method, String url) {
+    private String httpMethod;  
+
+    public AnnotationMapping(Class<?> clazz, Method method, String url, String httpMethod) {
         this.clazz = clazz;
         this.method = method;
         this.url = url;
+        this.httpMethod = httpMethod;
     }
     
     public Class<?> getClazz() {
         return clazz;
     }
+    
     public void setClazz(Class<?> clazz) {
         this.clazz = clazz;
     }
+    
     public Method getMethod() {
         return method;
     }
+    
     public void setMethod(Method method) {
         this.method = method;
     }
+    
     public String getUrl() {
         return url;
     }
+    
     public void setUrl(String url) {
         this.url = url;
     }
-     @Override
+    
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+    
+    public void setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+    
+    @Override
     public String toString() {
-        return "Handler{" +
+        return "AnnotationMapping{" +
                 "clazz=" + clazz.getName() +
                 ", method=" + (method != null ? method.getName() : "None") +
-                ", Url='" + url + '\'' +
+                ", url='" + url + '\'' +
+                ", httpMethod='" + httpMethod + '\'' +
                 '}';
     }
 }
